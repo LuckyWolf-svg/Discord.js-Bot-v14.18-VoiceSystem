4a0d6c7cb9c75557ce1b8e574a18008bca1bca3e
LuckyWolf-svg <134198480+LuckyWolf-svg@users.noreply.github.com>
2026-01-08 01:25:12 +0700

    Update VoiceChatClear.js

--- Events/VoiceChatClear.js
+++ Events/VoiceChatClear.js
@@ -1,12 +1,12 @@
 import { REST, Routes, EmbedBuilder, Client, Events, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
+import { CHANNEL_IDS } from '../server_ids.js';
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 export function registerVoiceChatClear(client) {
     const messageQueue = new Set();
-    const channelId = 'ChannelWhereNeedDeleteText';
-    
+
     client.on('messageCreate', message => {
-        if (message.channel.id === channelId && message.author.id != 'BotID') {
+        if (message.channel.id === CHANNEL_IDS.VOICE_CREATE && message.author.id != CHANNEL_IDS.BOT_ID) {
             messageQueue.add(message);
         }
     });
